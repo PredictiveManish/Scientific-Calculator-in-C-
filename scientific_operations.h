9dcd174476b878b0c9b96f1c7a367f9d1434e16b
Manish Tiwari <77712914+PredictiveManish@users.noreply.github.com>
2024-08-31 10:41:07 +0530

    Add files via upload

---
+++ scientific_operations.h
@@ -0,0 +1,12 @@
+#ifndef SCIENTIFIC_OPERATIONS_H
+#define SCIENTIFIC_OPERATIONS_H
+
+#include <cmath>
+
+double power(double base, double exponent);
+double squareRoot(double value);
+double sine(double angle);
+double cosine(double angle);
+double tangent(double angle);
+
+#endif // SCIENTIFIC_OPERATIONS_H
